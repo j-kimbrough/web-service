353a74c39d325385ba67f97f9fe5183e5409003b
Jade Kimbrough <jadekimbrough123@gmail.com>
2022-02-17 09:20:50 -0800

    adds php and replit files

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>PHP File!</title>
+    </head>
+    <body>
+        <h1>PHP File!</h1>
+
+        <p> This is HTML!</p>
+
+        <?php
+        echo "<p> this is PHP!</p>"; 
+        ?>
+
+        <p> this is HTML!</p>
+        
+    </body>    
+</html>
