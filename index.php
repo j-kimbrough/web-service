12a73055b1bb4048238f82f8f4bebd7af7f84a91
Jade Kimbrough <jadekimbrough123@gmail.com>
2022-02-22 17:22:57 +0000

    changes htm to php and use that index page and added error handler

--- index.php
+++ index.php
@@ -1,18 +1,66 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>PHP File!</title>
-    </head>
-    <body>
-        <h1>PHP File!</h1>
 
-        <p> This is HTML!</p>
+<head>
+	<title>Bond Web Service Demo</title>
+	<style>
+		body {
+			font-family: georgia;
+		}
+	</style>
+	<script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
-        <?php
-        echo "<p> this is PHP!</p>"; 
-        ?>
+	<script type="text/javascript">
+		$(document).ready(function () {
 
-        <p> this is HTML!</p>
+			$('.category').click(function (e) {
+				e.preventDefault(); //stop default action of the link
+				cat = $(this).attr("href");  //get category from URL
 				
-        <p> Hello from repl.it</p>
-    </body>    
+				var request = $.ajax({
+					url: "apiZZZZ.php?cat="+cat,
+					method: "GET",
+					dataType: "Json"
+					});
+				request.done(function( data ) {
+					console.log(data);
+					});
+				request.fail(function( xhr, status, error ) {
+					   //Ajax request failed.
+               var errorMessage = xhr.status + ': ' + xhr.statusText
+               alert('Error - ' + errorMessage);
+					});
+				
+				loadAJAX(cat);  //load AJAX and parse JSON file
+			});
+		});
+
+
+		function loadAJAX(cat) {
+			//AJAX connection will go here
+			alert('cat is: ' + cat);
+		}
+
+		function toConsole(data) {//return data to console for JSON examination
+			console.log(data); //to view,use Chrome console, ctrl + shift + j
+		}
+
+		function bondJSON(data) {
+			//JSON processing data goes here
+		}
+
+	</script>
+</head>
+
+<body>
+	<h1>Bond Web Service</h1>
+	<a href="year" class="category">Bond Films By Year</a><br />
+	<a href="box" class="category">Bond Films By International Box Office Totals</a>
+	<h3 id="filmtitle">Title Will Go Here</h3>
+	<div id="films">
+		<p>Films will go here</p>
+	</div>
+	<div id="output">Results go here</div>
+</body>
+
 </html>
